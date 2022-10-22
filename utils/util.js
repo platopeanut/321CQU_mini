db808b2cc98ae54315668b2be4911d6d05746464
Peanut <2857177614@qq.com>
2022-10-23 00:52:09 +0800

    modified version and info

--- utils/util.js
+++ utils/util.js
@@ -1,6 +1,6 @@
 // noinspection JSNonASCIINames,NonAsciiCharacters
-const CurrentVersion = '2.5.3'
-const CurrentVersionInfo = "更新内容：\n1. 恢复空教室查询"
+const CurrentVersion = '2.6.0'
+const CurrentVersionInfo = "更新内容：\n1.新增体测成绩查询\n2. 恢复空教室查询\n3. 优化界面"
 const log = require('./log')
 const dormitory = {
     'A区': ['选择楼栋','一舍学生宿舍','二舍学生宿舍','三舍学生宿舍','四舍学生宿舍','五舍学生宿舍','六舍学生宿舍','七舍学生宿舍','八舍学生宿舍','九舍学生宿舍','十舍学生宿舍','十一舍学生宿舍','十二舍学生宿舍','A栋宿舍','C栋宿舍','D栋宿舍'],
